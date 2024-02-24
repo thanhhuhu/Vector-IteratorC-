d5854e6b2de158c3695468006f1cd30ed809cd73
thanhhuhu <137694558+thanhhuhu@users.noreply.github.com>
2024-02-24 17:36:21 +0300

    Add files via upload

---
+++ Set.cpp
@@ -0,0 +1,64 @@
+﻿#include<iostream>
+#include<set>
+#include<vector>
+// set = log n
+using ll = long long;
+using namespace std;
+int main()
+{
+	//set<int> se;// {}
+	//se.insert(2);//{2}
+	//se.insert(3);//{2,3}
+	//se.insert(1);//{1,2,3}
+	//se.insert(2);//{1,2,3} không thể insert thêm giá trị 2 vì trùng trong dãy 
+	//se.insert(0);//{0,1,2,3}
+	//se.insert(4);//{0,1,2,3,4}
+	//cout << se.size() << endl;
+	//// xuất ra các phần tử của dãy 
+	//for (int x : se)
+	//{
+	//	cout << x << ' ';
+	//}
+	//// xuat ra bang iterator
+	
+	// set tối ưu ở việc nó chỉ giữ lại những phần tử khác nhau
+	// loại bỏ những phần tử khác nhau 
+	// xuất ra dãy sẽ tự động theo thứ tự tăng dần 
+	// tìm kiếm nhanh ologn
+	int n;
+	cin >> n;
+	set<int> se;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		se.insert(x);
+	}
+	cout << se.size() << endl;
+	// hàm trong set : find
+	//kiểm tra sự tồn tại của một phần tử nào đó trong set 
+	//hàm trả về iterator tới phần tử nếu nó tìm thấy;
+	set<int> se;
+	int a[5] = { 1,2,3,4,5 };
+	for (int i = 0; i < 5; i++)
+	{
+		se.insert(a[i]);
+	}
+	auto it = se.find(3);
+	if (it == se.end())// trỏ đến sau phần tử cuối 
+	{
+		cout << "Not found!" << endl;
+	}
+	else {
+		cout << "Found!" << endl;
+	}
+	// hàm count để đếm số lần xuất hiện 
+	cout << se.count(3) << endl;
+	se.erase(3);
+	//xóa phần tử 
+	// nếu xóa mà không tồn tại trong set đó thì sẽ gây lỗi 
+	// ví dụ như 1 2 3 4 5 xóa 6 sẽ gây lỗi runtime error 
+	for (int x : se)
+		cout << x << " ";
+
+}
---
+++ pair.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<vector>
+using ll = long long;
+using namespace std;
+int main()
+{
+	// <first, second> 
+	// pair<int, char>
+	//pair<int, int> p = make_pair(10, 20);
+	// pair <int,int> p ={10,20};
+	// c++11 tro len 
+	// tạo thành phần thứ nhất là 10 , thành phần thứ 2 là 20
+	/*pair <int, int > p1 = p;*/
+	//cout << p.first << " " << p.second << endl;
+
+	int n;
+	cin >> n;
+	pair<int, int > a[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i].first >> a[i].second;
+	}
+	vector<pair<int, int>> v;
+	pair<int ,pair<int, int>> p;
+	p.first = 10;
+	p.second.first = 20;
+	p.second.second = 30;//{10,{20,30} }
+}
